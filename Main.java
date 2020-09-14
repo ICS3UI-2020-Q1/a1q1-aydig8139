fac17a584ee9a3878f077e3061ac488a84fdffa7
aydig8139 <aydig8139@wrdsb.ca>
2020-09-14 16:51:10 +0000

    Finished the conversion problem

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * A program that will ask the user for a temperature in degrees Celsius and convert it into degrees Fahrenheit.
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Creates a Scanner used for input
+    Scanner input = new Scanner(System.in);
+    
+    // Ask the user for the temperature they would like to convert to fahrenheit
+    System.out.println("Please enter a temperature you would like to convert in degrees Celsius.");
+
+    // Get input temperature from user
+    int degreesC = input.nextInt();
+
+    // Declare and intialize a variable using the input from the user
+    int degreesF = (degreesC*9/5+32);
+
+    // Tell the user the fahrenheit version of the temperature input
+    System.out.println(degreesC + "C is the same as " + degreesF + "F");
+
     
   }
 }
